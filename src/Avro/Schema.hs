b3116290a246539e55d15df0158eebf3b4b4c778
Benjamin Mellor <ben@smokingkangaroo.com>
2015-01-31 17:31:02 +1100

    very simple demo final encoding

--- src/Avro/Schema.hs
+++ src/Avro/Schema.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE TupleSections
+           , TypeSynonymInstances
+  #-}
+
 module Avro.Schema
   (
   )
 where
 
+import Data.Bool (bool)
 
+
+class Schema t
+  where avroNull :: t ()
+        avroBool :: t Bool
+
+
+newtype Decoder t = Decoder { decode :: String -> (String, t) }
+
+instance Schema Decoder
+  where avroNull = Decoder (, ())
+
+        avroBool = Decoder f
+          where f ('\NUL':cs) = (cs, False)
+                f (_:cs) = (cs, True)
+
+
+newtype Encoder t = Encoder { encode :: t -> String }
+
+instance Schema Encoder
+  where avroNull = Encoder (const "")
+
+        avroBool = Encoder (bool "\NUL" "\1")
