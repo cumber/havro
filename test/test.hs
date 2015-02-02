bbfa4f6fa9a1b2dd4212a259f4838ceb226b6f2c
Benjamin Mellor <ben@smokingkangaroo.com>
2015-02-02 14:50:09 +1100

    use explicit Series for int8 rather than orphan instance

--- test/test.hs
+++ test/test.hs
@@ -5,12 +5,13 @@
 
 import Control.Applicative ((<$>))
 
+import Data.Bool (bool)
 import Data.Int (Int8)
 
-import Test.SmallCheck.Series (Serial, series)
+import Test.SmallCheck.Series ((\/), Series, generate)
 
 import Test.Tasty (TestTree, defaultMain, testGroup)
-import Test.Tasty.SmallCheck ((==>))
+import Test.Tasty.SmallCheck ((==>), over)
 import qualified Test.Tasty.SmallCheck as SC
 
 import ZigZagCoding
@@ -27,22 +28,36 @@ tests = testGroup "Tests" [zigZagTests]
 zigZagTests :: TestTree
 zigZagTests = testGroup "Zig zag coding"
   [ SC.testProperty "decode . encode == id"
-      ( \x -> (== x) . zigZagDecode . zigZagEncode $ (x :: Int8) )
+      ( over allInt8s
+          $ \x -> (== x) . zigZagDecode . zigZagEncode $ (x :: Int8)
+      )
 
   , SC.testProperty "encode (x + signum x) == (encode x) + 2"
-      ( \x ->   not (x `elem` [0, minBound, maxBound])
-                ==> zigZagEncode (x + signum x)
-                    == (zigZagEncode (x :: Int8)) + 2
+      ( over allInt8s
+          $ \x ->   not (x `elem` [0, minBound, maxBound])
+                    ==> zigZagEncode (x + signum x)
+                        == (zigZagEncode (x :: Int8)) + 2
       )
 
   , SC.testProperty "abs x < abs y => encode x < encode y"
-      ( \x ->  x /= minBound
-            ==> \y -> abs x < abs (y :: Int8)
-                    ==> zigZagEncode x < zigZagEncode y
+      ( over allInt8s
+          $ \x ->   x /= minBound
+                    ==> over allInt8s
+                          $ \y ->   abs x < abs (y :: Int8)
+                                    ==> zigZagEncode x < zigZagEncode y
       )
   ]
 
 
+-- Generator ignores depth and just fully explores Int8
+allInt8s :: Monad m => Series m Int8
+allInt8s = generate $ const [minBound..maxBound]
 
-instance Monad m => Serial m Int8
-  where series = (\x -> fromIntegral (x :: Int)) <$> series
+int8s :: Monad m => Series m Int8
+int8s = maybe0 \/ positive \/ negative
+  where maybe0 = generate $ \d -> bool [0] [] $ d >= 0
+        positive = generate $ \d -> [1 .. toInt8 d]
+        negative = negate <$> positive
+
+        toInt8 :: Int -> Int8
+        toInt8 d = fromIntegral $ min d (fromIntegral (maxBound :: Int8))
