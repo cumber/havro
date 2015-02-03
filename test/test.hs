29baee12d72aa989b237c5f9f72850bf78ec12f6
Benjamin Mellor <ben@smokingkangaroo.com>
2015-02-03 22:13:50 +1100

    add roundtrip tests for all implemented types

--- test/test.hs
+++ test/test.hs
@@ -1,19 +1,29 @@
 {-# LANGUAGE FlexibleInstances
+           , LambdaCase
            , MultiParamTypeClasses
            , NegativeLiterals
+           , RankNTypes
   #-}
 
 import Control.Applicative ((<$>))
 
 import Data.Bool (bool)
-import Data.Int (Int8)
+import Data.Int (Int8, Int32, Int64)
+
+import Data.Attoparsec.ByteString.Lazy (Parser, Result(Done, Fail), parse)
+import Data.ByteString.Builder (toLazyByteString)
+import Data.ByteString.Lazy (ByteString)
+
+import GHC.Exts (fromString)
 
 import Test.SmallCheck.Series ((\/), Series, generate)
 
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.SmallCheck ((==>), over, testProperty)
 
-import ZigZagCoding
+import ZigZagCoding (zigZagEncode, zigZagDecode)
+
+import Avro.Schema (Schema, avroNull, avroBool, avroInt, avroLong, avroString, encode)
 
 
 main :: IO ()
@@ -21,7 +31,10 @@ main = defaultMain tests
 
 
 tests :: TestTree
-tests = testGroup "Tests" [zigZagTests]
+tests = testGroup "Tests"
+  [ zigZagTests
+  , avroPrimitiveTests
+  ]
 
 
 zigZagTests :: TestTree
@@ -52,6 +65,34 @@ zigZagTests = testGroup "Zig zag coding"
   , testProperty "negatives encode to odd"
       ( over allInt8s $ \x -> x < 0 ==> odd (zigZagEncode x) )
   ]
+
+
+avroPrimitiveTests :: TestTree
+avroPrimitiveTests = testGroup "Avro primitives"
+  [ testProperty "Null: decode . encode == id" (roundTrip avroNull)
+
+  , testProperty "Bool: decode . encode == id" (roundTrip avroBool)
+
+  , testProperty "Int: decode . encode == id"
+      ( roundTrip avroInt . (fromIntegral :: Int -> Int32) )
+
+  , testProperty "Long: decode . encode == id"
+      ( roundTrip avroLong . (fromIntegral :: Int -> Int64) )
+
+  , testProperty "String: decode . encode == id"
+      ( roundTrip avroString . fromString )
+  ]
+
+
+roundTrip :: Eq a => (forall s. Schema s => s a) -> a -> Bool
+roundTrip s = \x -> parses x s . toLazyByteString . encode s $ x
+
+
+parses :: Eq a => a -> Parser a -> ByteString -> Bool
+parses x parser
+  = \case   Fail {} -> False
+            Done _ r -> x == r
+    . parse parser
 
 
 -- Generator ignores depth and just fully explores Int8
